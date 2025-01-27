8652d12089a16cb53677adadc139b984fa85f13a
Juan20Daniel <juandaniel200031@gmail.com>
2025-01-27 16:26:18 -0600

    Diseño

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CalculadoraComponent } from './calculadora/calculadora.component';
+import { ScreenComponent } from './calculadora/components/screen/screen.component';
+import { ButtonComponent } from './calculadora/components/button/button.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CalculadoraComponent,
+    ScreenComponent,
+    ButtonComponent
   ],
   imports: [
     BrowserModule,
